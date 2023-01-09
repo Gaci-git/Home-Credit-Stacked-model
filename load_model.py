99b3e2599de29d1f0124a26346de2ced23026ae6
Gaci-git <69762251+Gaci-git@users.noreply.github.com>
2023-01-09 18:39:48 +0200

    Delete load_model.py

--- load_model.py
+++
@@ -1,8 +0,0 @@
-
-model = None
-
-def load_model():
-    global model
-    # model variable refers to the global variable
-    with open('stacked_model.pkl', 'rb') as f:
-        model = pickle.load(f)
